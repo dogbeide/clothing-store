dcbebb5555222887a158f6700018ea3d82c829d2
David Ogbeide <boyowadavid@gmail.com>
2023-10-27 23:19:36 -0400

    implement routing

---
+++ src/routes/navbar/navbar.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import './navbar.styles.scss'
+import Logo from '../../assets/reeftipus_41x38.svg'
+
+const NavBar = () => {
+  return (
+    <Fragment>
+      <div className='navbar'>
+        <Link className="logo-container" to='/'>
+          <img className='logo' src={Logo} alt="reeftipus logo" />
+        </Link>
+        <div className="navbar-links">
+          <Link className="navbar-link" to='/shop'>
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+
+export default NavBar;
