e1ced438853d7daf5ad1bc555c00cc724faf55e9
David Ogbeide <boyowadavid@gmail.com>
2023-11-03 16:42:35 -0400

    create sign-up-component and button-component

--- src/components/sign-up/sign-up-form.component.jsx
+++ src/components/sign-up/sign-up-form.component.jsx
@@ -5,6 +5,8 @@ import {
   createUserDocumentFromAuth 
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
   'displayName': '',
@@ -55,15 +57,16 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <h1>Sign Up with Email and Password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign Up with Email and Password</span>
       <form onSubmit={handleSubmit}>
         <FormInput label='Display Name' type="text" onChange={handleChange} name="displayName" value={displayName} required />
         <FormInput label='Email' type="email" onChange={handleChange} name="email" value={email} required />
         <FormInput label='Password' type="password" onChange={handleChange} name="password" value={password} required />
         <FormInput label='Confirm Password' type="password" onChange={handleChange} name="confirmPassword" value={confirmPassword} required />
 
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
         {errorMsg ? <div style={{'color': 'red'}}>{errorMsg}</div> : null}
       </form>
     </div>
