f2a615204f5c81a62d4e21e1fc6146b9b9277467
David Ogbeide <boyowadavid@gmail.com>
2023-11-02 23:49:44 -0400

    genericize form input component & style 'em

--- src/components/sign-up/sign-up-form.component.jsx
+++ src/components/sign-up/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import {
   createAuthUserWithEmailAndPassword, 
   createUserDocumentFromAuth 
 } from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
 
 const defaultFormFields = {
   'displayName': '',
@@ -53,17 +54,10 @@ const SignUpForm = () => {
     <div>
       <h1>Sign Up with Email and Password</h1>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input type="text" onChange={handleChange} name="displayName" value={displayName} required />
-
-        <label>Email</label>
-        <input type="email" onChange={handleChange} name="email" value={email} required />
-
-        <label>Password</label>
-        <input type="password" onChange={handleChange} name="password" value={password} required />
-
-        <label>Confirm Password</label>
-        <input type="password" onChange={handleChange} name="confirmPassword" value={confirmPassword} required />
+        <FormInput label='Display Name' type="text" onChange={handleChange} name="displayName" value={displayName} required />
+        <FormInput label='Email' type="email" onChange={handleChange} name="email" value={email} required />
+        <FormInput label='Password' type="password" onChange={handleChange} name="password" value={password} required />
+        <FormInput label='Confirm Password' type="password" onChange={handleChange} name="confirmPassword" value={confirmPassword} required />
 
         <button type="submit">Sign Up</button>
         {errorMsg ? <div style={{'color': 'red'}}>{errorMsg}</div> : null}
