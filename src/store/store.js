22947147e239822d2e2fae19cfe50298f82cae9a
David Ogbeide <boyowadavid@gmail.com>
2023-11-20 00:15:57 -0500

    resume redux-logger logger + refactor custom logger into separate file/folder

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,12 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import logger from "redux-logger";
+import logger from "redux-logger";
+// import logger from "./middleware/logger";
 
 import { rootReducer } from "./root-reducer";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
 
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -27,7 +16,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(
