1990bd8b859eadb34a1f8e1bebad2e37b31cd041
David Ogbeide <boyowadavid@gmail.com>
2023-11-20 00:13:25 -0500

    persist cart and shop data in web browser's local storage between sessions

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App.jsx'
 import './index.scss'
-import { store } from './store/store.js'
+import { store, persistor } from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   // </React.StrictMode>,
 )
