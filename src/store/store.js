554e9f6fb1bdafde80ef4f485e73546de5f12785
David Ogbeide <boyowadavid@gmail.com>
2023-11-19 23:59:04 -0500

    remove currying commenting

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,7 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-
-/* CURRYING */
-/* FOO THAT RETURNS ANOTHER FOO */
-/* fn generator */
-/* add parameters in stages */
-
-// const curryFoo = (a) => (b, c) => {
-//   a + b - c
-// }
-
-// const with3 = curryFoo(3); // (b, c) => { 3 + b - c}
-// const with10 = curryFoo(10); // (b, c) => { 10 + b - c
-
-// with3(2, 4); // 3 + 2 - 4
 
 const ourLoggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
